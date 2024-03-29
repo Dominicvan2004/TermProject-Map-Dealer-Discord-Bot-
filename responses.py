9c2b543273e2fbccd5e405dbb1a6248892a05266
Dominicvan2004 <163356198+Dominicvan2004@users.noreply.github.com>
2024-03-29 12:14:49 -0700

    Kinda finished again
    i took out old functions from the tutorial

--- responses.py
+++ responses.py
@@ -2,12 +2,6 @@ from random import choice, randint
 
 def get_responses(user_input: str) -> str:
   lowered: str = user_input.lower()
-
-  if lowered == '':
-    return 'Noting dear'
-  elif 'hello' in lowered:
-    return 'Hello there'
-  
   
   if lowered == "mapplz":
     return f'https://osu.ppy.sh/beatmapsets/{randint(1,1075607)}#osu/{randint(1,2250670)}'
