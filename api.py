5a45a22a15a11ac92ea7785827bb3edb663f6eef
Dominicvan2004 <163356198+Dominicvan2004@users.noreply.github.com>
2024-11-29 03:29:36 -0800

    Trying out the osu api and fixed my API key being in an actual file ToT

---
+++ api.py
@@ -0,0 +1,49 @@
+from osu import Client, BeatmapsetEventType, BeatmapsetEventSort
+from datetime import datetime
+from dotenv import load_dotenv
+from osu import (
+    Client,
+    BeatmapsetSearchFilter as Filter,
+    BeatmapsetSearchStatus as Status,
+    BeatmapsetSearchGeneral as General,
+    BeatmapsetSearchExtra as Extra,
+    BeatmapsetLanguage as Language,
+    BeatmapsetGenre as Genre
+)
+import os
+
+load_dotenv()
+
+client_id = int(os.getenv('CLIENT_ID'))
+client_secret = os.getenv('CLIENT_SECRET')
+redirect_url = os.getenv('REDIRECT_URL')
+
+client = Client.from_client_credentials(client_id, client_secret, redirect_url)
+
+
+default_search_result = client.search_beatmapsets()
+recently_qualified = client.search_beatmapsets(
+    Filter()
+    .set_status(Status.QUALIFIED)
+)
+japanese_ranked = client.search_beatmapsets(
+    Filter()
+    .set_language(Language.JAPANESE)
+    .set_status(Status.RANKED)
+
+)
+russian_rock_loved = client.search_beatmapsets(
+    Filter()
+    .set_language(Language.ENGLISH)
+    .set_genre(Genre.METAL)
+    .set_status(Status.LOVED)
+)
+english_featured_artists_has_video_and_storyboard_including_converts = client.search_beatmapsets(
+    Filter()
+    .set_language(Language.ENGLISH)
+    .set_generals([General.FEATURED_ARTISTS, General.CONVERTS])
+    .set_extra([Extra.VIDEO, Extra.STORYBOARD])
+    .set_status(Status.ANY)
+)
+
+print(f"Default: {default_search_result}\n")
--- responses.py
+++ responses.py
@@ -1,12 +1,34 @@
 from random import choice, randint
+from dotenv import load_dotenv
+from osu import (
+    Client,
+    BeatmapsetSearchFilter as Filter,
+    BeatmapsetSearchStatus as Status,
+    BeatmapsetSearchGeneral as General,
+    BeatmapsetSearchExtra as Extra,
+    BeatmapsetLanguage as Language,
+    BeatmapsetGenre as Genre
+)
+import os
+
+load_dotenv()
+
+client_id = int(os.getenv('CLIENT_ID'))
+client_secret = os.getenv('CLIENT_SECRET')
+redirect_url = os.getenv('REDIRECT_URL')
+
+client = Client.from_client_credentials(client_id, client_secret, redirect_url)
+
 # This gets the responses within the discord chat log
 def get_responses(user_input: str) -> str:
   lowered: str = user_input.lower()
   
-  # This checks for a specific response and if the response matches "mapplz" it returns a random beatmap
-  if lowered == "mapplz":
-    return f'https://osu.ppy.sh/beatmapsets/{randint(1,1075607)}#osu/{randint(1,2250670)}'
-
-
-    
   
+
+ # f'https://osu.ppy.sh/beatmapsets/{randint(1,1075607)}#osu/{randint(1,2250670)}'
+  # This checks for a specific response and if the response matches "mapplz" it returns a random beatmap
+
+
+  beatmap = client.get_beatmapset(4635746)
+  if lowered == "mapplz":
+    return beatmap
