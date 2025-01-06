fb1bf48faa17c7ea9ba4d59966ce9da66cfa4004
Dominicvan2004 <163356198+Dominicvan2004@users.noreply.github.com>
2025-01-05 21:33:41 -0800

    merge with mathew

--- responses.py
+++ responses.py
@@ -19,16 +19,32 @@ redirect_url = os.getenv('REDIRECT_URL')
 
 client = Client.from_client_credentials(client_id, client_secret, redirect_url)
 
-# This reads the chat log and finds the key word "mapplz" then finds a random map via questionably creative method
+# This gets the responses within the discord chat log
 def get_responses(user_input: str) -> str:
-  lowered: str = user_input.lower()
-  if lowered == "mapplz":
-    while 1:
-        try:
-            newID = randint(1,100)
-            beatmapset = client.get_beatmapset(newID)
-            if beatmapset.ranked == 1:
-                return(f"https://osu.ppy.sh/beatmapsets/{newID}")
+
+#use the search beatmap fucntion to get the url of a ranked beat map 
+
+    rank_List = [1,2,3,4]
+    lowered: str = user_input.lower()
+
+    if lowered == "mapplz":
+        while 1:
+            beatmapset = client.get_beatmap(randint(1,3894959))
+            if beatmapset.ranked in rank_List:
+                return(beatmapset.url)
                 break
-        except:
-            continue
+
+
+# This reads the chat log and finds the key word "mapplz" then finds a random map via questionably creative method
+# def get_responses(user_input: str) -> str:
+#   lowered: str = user_input.lower()
+#   if lowered == "mapplz":
+#     while 1:
+#         try:
+#             newID = randint(1,100)
+#             beatmapset = client.get_beatmapset(newID)
+#             if beatmapset.ranked == 1:
+#                 return(f"https://osu.ppy.sh/beatmapsets/{newID}")
+#                 break
+#         except:
+#             continue
